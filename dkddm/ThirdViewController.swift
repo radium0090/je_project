b0c53579b0b80c723a73cf43df8cd8b0678d5fbd
shi.rei <shi.rei@vega-c.com>
2018-12-18 10:55:29 +0900

    first commit

---
+++ dkddm/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  dkddm
+//
+//  Created by 是レイ on 2018/12/18.
+//  Copyright © 2018年 SHI LEI. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ThirdViewController: UIViewController, WKUIDelegate {
+    
+    private var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // WKWebViewを生成
+        webView = WKWebView(frame:CGRect(x:0, y:0, width:self.view.bounds.size.width, height:self.view.bounds.size.height))
+        
+        // URL設定
+        let urlString = "https://www.baidu.com/"
+        let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        
+        let url = NSURL(string: encodedUrlString!)
+        let request = NSURLRequest(url: url! as URL)
+        
+        webView.load(request as URLRequest)
+        
+        self.view.addSubview(webView)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ dkddmTests/dkddmTests.swift
@@ -0,0 +1,34 @@
+//
+//  dkddmTests.swift
+//  dkddmTests
+//
+//  Created by 是レイ on 2018/12/18.
+//  Copyright © 2018年 SHI LEI. All rights reserved.
+//
+
+import XCTest
+@testable import dkddm
+
+class dkddmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
