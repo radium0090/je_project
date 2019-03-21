860f73adb839bb402e8c5a4a4d32b2efe5f8fb9d
皮皮 <pp@pipideMacBook-Pro.local>
2019-03-21 11:21:03 +0900

    add new tab

---
+++ dkddm/Controller/FouViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FouViewController.swift
+//  dkddm
+//
+//  Created by 皮皮 on 2019/03/16.
+//  Copyright © 2019 SHI LEI. All rights reserved.
+//
+
+import UIKit
+
+class FouViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
